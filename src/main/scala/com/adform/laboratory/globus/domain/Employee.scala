3970dca7e249600896fc6854bdc5a6e3a66bbd4d
breedish@gmail.com <lithiumish4~>
2015-02-21 11:49:12 +0300

    code review changes

--- src/main/scala/com/adform/laboratory/globus/domain/Employee.scala
+++ src/main/scala/com/adform/laboratory/globus/domain/Employee.scala
@@ -3,9 +3,4 @@ package com.adform.laboratory.globus.domain
 /** @author zenind */
 case class Employee(id: String, firstName: String, lastName: String, roles: Set[Role],
   icon: String, twitter:String, email: String, bio: String, pod: Pod) {
-  require(firstName != null && lastName != null)
-  require(roles.nonEmpty)
-
-  def assignRole(role: Role) = roles + role
-  def removeRole(role:Role) = roles - role
 }
--- src/main/scala/com/adform/laboratory/globus/services/EmployeeServiceImpl.scala
+++ src/main/scala/com/adform/laboratory/globus/services/EmployeeServiceImpl.scala
@@ -1,11 +1,16 @@
 package com.adform.laboratory.globus.services
 
-import com.adform.laboratory.globus.domain.Employee
+import com.adform.laboratory.globus.domain.{Pod, Role, Employee}
 
 /** @author zenind */
 class EmployeeServiceImpl(storage: EntityRepository[String, Employee]) {
-  require(storage != null)
 
   def saveEmployee(e: Employee): Employee = storage.save(e)
+
+  def assignEmployeeRole(e: Employee, role: Role):Employee = e.copy(roles = e.roles + role)
+  def removeEmployeeRole(e: Employee, role:Role):Employee = e.copy(roles = e.roles - role)
+
+  def assignEmployee(p: Pod, e: Employee) = p.copy(employees = p.employees + e)
+  def unAssignEmployee(p: Pod, e: Employee) = p.copy(employees = p.employees - e)
 
 }
