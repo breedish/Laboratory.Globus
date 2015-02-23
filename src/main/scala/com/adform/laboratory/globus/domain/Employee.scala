c57f9bcd67c94cf1730ec68f97454e63a5bdfa7b
breedish@gmail.com <lithiumish4~>
2015-02-23 22:17:31 +0300

    Code review changes

--- src/main/scala/com/adform/laboratory/globus/domain/Employee.scala
+++ src/main/scala/com/adform/laboratory/globus/domain/Employee.scala
@@ -2,5 +2,4 @@ package com.adform.laboratory.globus.domain
 
 /** @author zenind */
 case class Employee(id: String, firstName: String, lastName: String, roles: Set[Role],
-  icon: String, twitter:String, email: String, bio: String, pod: Pod) {
-}
+  icon: String, twitter:String, email: String, bio: String, pod: Pod)
